4b13cf719143a8bec18882c0279972924baf30a2
MatthewAlgo <matthewdy0101@gmail.com>
2022-09-01 14:59:39 +0300

    Added basic functionality

--- lib/utilities/youtube.dart
+++ lib/utilities/youtube.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 import '../views/homepage.dart';
@@ -6,16 +7,22 @@ Future<StreamManifest> getManifest(String vidID) async {
   HomePage.loadingState = true;
   var yt = YoutubeExplode();
   var streamInfo = await yt.videos.streamsClient.getManifest('vidID');
-  
+
   HomePage.loadingState = false;
   return streamInfo;
 }
 
-Future<VideoSearchList> getSearch(String searchquery) async {
+Future<VideoSearchList> getSearch(
+    String searchquery, BuildContext context) async {
   HomePage.loadingState = true;
   var yt = YoutubeExplode();
-  var search = await yt.search.search(searchquery);
-
+  late VideoSearchList search;
+  try {
+    search = await yt.search.search(searchquery);
+  } on Exception catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Something went wrong: ${e.toString()}")));
+  }
   HomePage.loadingState = false;
   return search;
 }
@@ -24,7 +31,7 @@ Future<CommentsList?> getComments(Video? video) async {
   HomePage.loadingState = true;
   var yt = YoutubeExplode();
   var comments = await yt.videos.commentsClient.getComments(video!);
-  
+
   HomePage.loadingState = false;
   return comments;
 }
@@ -33,13 +40,12 @@ Future<VideoSearchList> appendToSearchList(VideoSearchList vs) async {
   HomePage.loadingState = true;
   var yt = YoutubeExplode();
   var next = await vs.nextPage();
-  next = await next?.nextPage();
   if (next != null) {
     for (var video in next.sublist(0)) {
       vs.add(video);
     }
   }
- 
+
   HomePage.loadingState = false;
   return vs;
 }
